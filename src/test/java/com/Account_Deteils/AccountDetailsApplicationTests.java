553e48785831dca19f4a458eb40a4020b8b01947
mahavir-k <mahaviryadav004@gmail.com>
2025-04-04 16:55:31 +0530

    account details

---
+++ src/test/java/com/Account_Deteils/AccountDetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Account_Deteils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountDetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
